5a078f45c166407cc12ae01de35faefaa87ffad8
crbikebike <chris@eatingrd.com>
2015-04-09 19:40:54 -0700

    Commit at 3.11

--- threeline.py
+++ threeline.py
@@ -1,18 +1,35 @@
 __author__ = 'ChrisKristen'
 
-def newLine():
-    print
 
-def threeLines():
-    newLine()
-    newLine()
-    newLine()
+if __name__ == '__main__':
 
-def nineLines():
-    threeLines()
-    threeLines()
-    threeLines()
+    #functions and stuff
+    def newLine():
+        print
 
-print "sup"
-nineLines()
-print "not much"
+    def threeLines():
+        newLine()
+        newLine()
+        newLine()
+
+    def nineLines():
+        threeLines()
+        threeLines()
+        threeLines()
+
+    def printTwice(message):
+        print message, message
+
+    def concantMachine(firstMessage,secondMessage):
+        concantMsg = firstMessage + secondMessage
+        printTwice(concantMsg)
+
+      #variables
+    bottlecap = 'cats like dogs'
+
+      #actions and stuff
+    printTwice("'Spam*4'")
+
+    printTwice(bottlecap)
+
+    concantMachine('hello','kitty')
