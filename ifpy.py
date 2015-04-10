749ca0d3b14ef7e414cf8f5b3bfcecd7ed19bd8a
crbikebike <chris@eatingrd.com>
2015-04-09 22:18:22 -0700

    Moved functions into ifpy so they can all be kept in one spot.

--- ifpy.py
+++ ifpy.py
@@ -1,20 +1,26 @@
+#This module holds functions that have been written for use when completing the
+#open courseware of intro to programming from MIT: http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-189-a-gentle-introduction-to-programming-using-python-january-iap-2011/readings/
+
 # Determine oat temp and number of pies
-
-#just gonna leave this here
-
 def foodFunction(oatsTemp, pies):
-    #oats temp
-    if oatsTemp <= 6:
-        print 'your oats are too cold'
-    elif oatsTemp <= 10:
-        print 'goldilocks just ate all your oats'
-    else:
-        print 'your oats are hotter than the sun'
-    #as long as there is a pie it will get a 'nom'
-    while pies > 0:
-        print 'nom' + ' I have eaten ' + str(pies) + ' pies'
-        pies -= 1
-    print 'the pie is gone'
+     #oats temp
+        if oatsTemp <= 6:
+            print 'your oats are too cold'
+        elif oatsTemp <= 10:
+            print 'goldilocks just ate all your oats'
+        else:
+            print 'your oats are hotter than the sun'
+        #as long as there is a pie it will get a 'nom'
+        while pies > 0:
+            print 'nom' + ' I have eaten ' + str(pies) + ' pies'
+            pies -= 1
+        print 'the pie is gone'
 
-#Ask the person running to enter butt size and number of pies
-foodFunction(input("how hot is your oatmeal?\n"), input("how many pies?\n"))
+# Tells you the id value the argument holds in computer's memory
+def catOrDog(animal):
+    memorySpace = id(animal)
+    print 'this will always say dog and fyi your argument id is ' + str(memorySpace)
+
+#Demonstrates when a loop is exited because a return value is true, or false if not found
+def allTheSums(x, list):
+
---
+++ mit6.01_chapter2.py
@@ -0,0 +1,11 @@
+__author__ = 'chris race'
+
+# the chapter "learning to program in python" from this link
+#http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-01sc-introduction-to-electrical-engineering-and-computer-science-i-spring-2011/unit-1-software-engineering/object-oriented-programming/MIT6_01SCS11_chap02.pdf
+
+#import modules
+import ifpy
+
+if __name__ == '__main__':
+    ifpy.foodFunction(4,8)
+    ifpy.catOrDog(88)
